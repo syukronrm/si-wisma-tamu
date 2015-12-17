101a7b34b3b11d4fedb2a6abd8bcc7d181e57778
incursio13 <andreas.galang1@gmail.com>
2015-12-17 17:43:30 +0700

    update

--- project/flamboyan.php
+++ project/flamboyan.php
@@ -57,6 +57,21 @@
       <div class="col-md-12" >
         <div class="panel panel-default" style="background-color:#eaeae1">
           <div class="panel-body">
+            <?php
+               if(isset($_GET['status']) && $_GET['status']=="sukses")
+               {
+                echo "<div class='alert alert-dismissable alert-success'>
+                <button type='button' class='close' data-dismiss='alert'>&times;</button>
+                <center><b>Tabel Berhasil Diupdate!</b></center>
+                </div>";
+               }
+               else if(isset($_GET['status']) && $_GET['status']=="gagal"){
+                echo "<div class='alert alert-dismissable alert-danger'>
+                <button type='button' class='close' data-dismiss='alert'>&times;</button>
+                <center><b>Tabel Gagal Diupdate!</b></center>z
+                </div>";
+               } 
+            ?>
             <table class="table table-hover table-bordered table-striped">
                <thead>
                 <tr>
@@ -64,6 +79,7 @@
                   <th style="text-align:center;">Jenis Kamar</th>
                   <th style="text-align:center;">Status Kamar</th>
                   <th style="text-align:center;">Pilih</th>
+                  <th style="text-align:center;">Edit</th>
                   <th style="text-align:center;">Delete</th> 
                 </tr>
               </thead>
@@ -72,7 +88,7 @@
                   include 'coba.php';
                   $query = "select kamar.id_kamar, jenis_kamar.nama_jenis, kamar.status_kamar
                             from kamar , wisma, jenis_kamar
-                            where kamar.id_wisma=wisma.id_wisma and kamar.id_jenis= jenis_kamar.id_jenis
+                            where kamar.id_wisma=wisma.id_wisma and kamar.id_jenis= jenis_kamar.id_jenis and wisma.nama_wisma='Flamboyan'
                             order by kamar.id_kamar";
                   $stid = oci_parse($conn, $query);
                   oci_execute($stid);
@@ -86,14 +102,25 @@
                     <td>
                       <div>
                         <form method="GET" action="<?php $_PHP_SELF ?>"> 
-                          <button type='submit'  name='id_kam' value="<?php echo $row['id_kamar'];?>">Edit</button>
+                          <?php 
+                          if ($row['STATUS_KAMAR'=='KOSONG'])
+                            $tombol= "btn btn-primary active";
+                          else  $tombol= "btn btn-primary";?>
+                          <button type='submit' class='$tombol' name='pilih_kam' value="<?php echo $row['ID_KAMAR'];?>">Pilih</button>
+                        </form>
+                      </div>
+                    </td>
+                    <td>
+                      <div>
+                        <form method="GET" action="<?php $_PHP_SELF ?>"> 
+                          <button type='submit' class="btn btn-primary" name='edit_kam' value="<?php echo $row['ID_KAMAR'];?>">Edit</button>
                         </form>
                       </div>
                     </td>
                     <td> 
                       <div>
                         <form method="GET" action="<?php $_PHP_SELF ?>"> 
-                          <button type='submit'  name='del_kam' value="<?php echo $row['id_kamar'];?>">Delete</button>
+                          <button type='submit' class="btn btn-primary" name='del_kam' value="<?php echo $row['ID_KAMAR'];?>">Delete</button>
                         </form>
                       </div>
                     </td>
@@ -103,16 +130,25 @@
                   ?>
               </tbody>
 
-               <?php
+            </table>
+            <div class="pull-right"> 
+              <a href="#">  <button class="btn">TAMBAH BARU</button> 
+            </a> </div>
+            <div> 
+              <a href="#">  <button class="btn">KEMBALI</button> 
+            </a> </div>
+
+            <div>
+             <?php
                 include "coba.php";
 
                 if( isset($_GET['del_kam']))
                 {
                   $idkam = $_GET['del_kam'];
                   echo ' 
-                     <form method="POST" action="delpegawai.php">
+                     <form method="POST" action="delkamar.php">
                       <div class="controls" style="display:none;">
-                        <input class="form-control" type="text" name="id_peg" value="'.$idpeg.'">
+                        <input class="form-control" type="text" name="id_kam" value="'.$idkam.'">
                       </div>
                       <div class="alert alert-dismissable">
                         <center><b> Melanjutkan Penghapusan ? </b> <br><br>
@@ -124,14 +160,6 @@
                   ';
                 }
              ?>
-
-            </table>
-            <div class="pull-right"> 
-              <a href="formtmbhpeg.php">  <button class="btn">TAMBAH BARU</button> 
-            </a> </div>
-            <div> 
-              <a href="kepegawaian.php">  <button class="btn">KEMBALI</button> 
-            </a> </div>
             
           </div>
         </div>

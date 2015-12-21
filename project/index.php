8d3b03b17e487036895466ac92609329c0ae4f75
syukronrm <syukronrifai@gmail.com>
2015-12-21 18:33:44 +0700

    Rename home.php ke index.php

---
+++ project/index.php
@@ -0,0 +1,18 @@
+<?php include("template/header.php");?>
+<?php include("template/navbar.php");?>
+
+<div class="container">
+  <div class="row" style="padding-top:20px;">
+    <div class="col-md-12" >
+      <div class="panel panel-default" style="background-color:#eaeae1">
+        <div class="panel-body">
+          <center> <h3><b>SELAMAT DATANG <br> SISTEM INFORMASI WISMA TAMU </b></h3>  <center>
+          <center><p> <img src="home.jpg" alt="Kepegawaian" style="height:300px"></center>
+          <br><br>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+<?php include("template/footer.php");?>

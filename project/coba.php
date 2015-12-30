83607eb7f2ca0043b514e9c01e1783b90e137333
syukronrm <syukronrifai@gmail.com>
2015-12-30 14:12:04 +0700

    punya syukron

--- project/coba.php
+++ project/coba.php
@@ -1,18 +1,13 @@
 <?php
 // Create connection to Oracle
-<<<<<<< HEAD
-$conn = oci_connect("FPFP", "FPFP", "//localhost/XE");
-=======
-$conn = oci_connect("fp", "fp", "//localhost/XE");
->>>>>>> c65de8546ace4113eb3c439c95c73ffd786f0b8e
+$conn = oci_connect("syukronrm", "sandi", "//localhost/XE");
 if (!$conn) {
    $m = oci_error();
    echo $m['message'], "\n";
    exit;
 }
 else {
-   
+
 }
 // Close the Oracle connection
-
 ?>

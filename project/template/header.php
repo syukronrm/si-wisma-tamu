c6c29043b74b8222963230ca48f5bf13d0e36295
syukronrm <syukronrifai@gmail.com>
2015-12-30 14:11:06 +0700

    Ganti bootstrap, soalnya datepicker gak cocok sama bootflat :((

--- project/template/header.php
+++ project/template/header.php
@@ -3,10 +3,10 @@
   <head>
     <title>Sistem Informasi Wisma Tamu</title>
     <meta name="viewport" content="width=device-width">
-        <link rel="stylesheet" href="css/site.min.css">
+        <link rel="stylesheet" href="css/bootstrap.css">
          <link rel="stylesheet" type="text/css" href="cssku.css">
     <script type="text/javascript" src="aset/jquery.min.js"></script>
-    <script type="text/javascript" src="js/site.min.js"></script>
+    <script type="text/javascript" src="js/bootstrap.js"></script>
     <style type="text/css">
 
     </style>

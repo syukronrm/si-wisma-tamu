c83d8ea42d7337c2063d11986eee40a863bdea9b
syukronrm <syukronrifai@gmail.com>
2015-12-31 14:52:52 +0700

    Fixing query

--- project/data-wisma.php
+++ project/data-wisma.php
@@ -1,18 +1,14 @@
 <?php include("template/header.php");?>
 <?php include("template/navbar.php");?>
 
-<<<<<<< HEAD
 <?php
   include 'coba.php';
 ?>
 
-=======
->>>>>>> 52ac95c6cf86fde11577fff93adb077269b7e58c
 <div class="container">
   <div class="row" style="padding-top:20px;">
     <div class="panel panel-default" style="background-color:#eee">
       <div class="panel-body">
-<<<<<<< HEAD
         <h3 align="center">DATA WISMA</h3>
         <?php
           if(isset($_GET['status']) && $_GET['status']=="sukses")
@@ -53,7 +49,7 @@
                 <td><?php echo $row['ID_WISMA'];?></td>
                 <td><?php echo $row['NAMA_WISMA'];?></td>
                 <td><?php echo $row['ALAMAT_WISMA'];?></td>
-                
+
                 <td>
                   <div>
                     <form method="POST" action="#">
@@ -108,58 +104,54 @@
               }
            ?>
          </div>
+
+         <h3 align="center">PENDAPATAN WISMA</h3>
+         <table class="table table-hover table-bordered table-striped">
+           <thead>
+             <tr>
+               <th style="text-align:center;">Wisma</th>
+               <th style="text-align:center;">Pendapatan Total</th>
+             </tr>
+           </thead>
+           <tbody>
+             <?php
+             include 'coba.php';
+             $query = "select temp2.nama_wisma, sum(temp2.harga) as pendapatan
+              from transaksi_sewakamar tr,
+              (
+                select m.id_transaksi, temp.nama_wisma, temp.harga
+                from menyewa m,
+                  (
+                  select k.id_kamar, w.nama_wisma, jk.harga
+                  from kamar k, jenis_kamar jk,
+                    (
+                      select nama_wisma, id_wisma
+                      from wisma
+                    ) w
+                  where k.id_wisma=w.id_wisma
+                  and k.id_jenis= jk.id_jenis
+                  ) temp
+                where m.id_kamar in temp.id_kamar
+              ) temp2
+              where temp2.id_transaksi=tr.id_transaksi
+              group by temp2.nama_wisma";
+             $stid = oci_parse($conn, $query);
+             oci_execute($stid);
+
+             while ($row = oci_fetch_array($stid))
+             {?>
+               <tr>
+                 <td><?php echo $row['NAMA_WISMA'];?></td>
+                 <td><?php echo $row['PENDAPATAN'];?></td>
+               </tr>
+             <?php
+             }
+             ?>
+           </tbody>
+         </table>
        </div>
      </div>
    </div>
 </div>
-=======
-        <table class="table table-hover table-bordered table-striped">
-          <thead>
-            <tr>
-              <th style="text-align:center;">Wisma</th>
-              <th style="text-align:center;">Pendapatan Total</th>
-            </tr>
-          </thead>
-          <tbody>
-            <?php
-            include 'coba.php';
-            $query = "select temp2.nama_wisma, sum(bayar) as pendapatan
-            from transaksi_sewakamar tr,
-            (
-              select m.id_transaksi, temp.nama_wisma
-              from menyewa m,
-                (
-                select k.id_kamar, w.nama_wisma
-                from kamar k,
-                  (
-                    select nama_wisma, id_wisma
-                    from wisma
-                  ) w
-                where k.id_wisma=w.id_wisma
-                ) temp
-              where m.id_kamar in temp.id_kamar
-            ) temp2
-            where temp2.id_transaksi=tr.id_transaksi
-            group by temp2.nama_wisma";
-            $stid = oci_parse($conn, $query);
-            oci_execute($stid);
-
-            while ($row = oci_fetch_array($stid))
-            {?>
-              <tr>
-                <td><?php echo $row['NAMA_WISMA'];?></td>
-                <td><?php echo $row['PENDAPATAN'];?></td>
-              </tr>
-            <?php
-            }
-            ?>
-          </tbody>
-        </table>
-      </div>
-    </div>
-  </div>
-</div>
-
->>>>>>> 52ac95c6cf86fde11577fff93adb077269b7e58c
 
 <?php include("template/footer.php");?>

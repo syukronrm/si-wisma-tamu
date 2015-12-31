ab6dbaffc6cd06252dccaed41528a54a74afe2c0
incursio13 <andreas.galang1@gmail.com>
2015-12-31 13:06:26 +0700

    test

--- project/coba.php
+++ project/coba.php
@@ -1,10 +1,10 @@
 <?php
 // Create connection to Oracle
-<<<<<<< HEAD
+
 $conn = oci_connect("FPFP", "FPFP", "//localhost/XE");
-=======
-$conn = oci_connect("fp", "fp", "//localhost/XE");
->>>>>>> c65de8546ace4113eb3c439c95c73ffd786f0b8e
+
+//$conn = oci_connect("fp", "fp", "//localhost/XE");
+
 if (!$conn) {
    $m = oci_error();
    echo $m['message'], "\n";

2f5212ed1476c764d8538f0d2e91a1507bc4a976
incursio13 <andreas.galang1@gmail.com>
2015-12-31 22:01:31 +0700

    edit date picker wisma bougenvile, jasmine

--- project/wisma-jasmine.php
+++ project/wisma-jasmine.php
@@ -1,6 +1,14 @@
 <?php include("template/header.php");?>
 <?php include("template/navbar.php");?>
 
+<link rel="stylesheet" type="text/css" href="css/datepicker.css">
+<script type="text/javascript" src="js/bootstrap-datepicker.js"></script>
+
+<?php
+  include 'coba.php';
+  session_start();
+?>
+
 <div class="container">
   <div class="row" style="padding-top:20px;">
     <div class="panel panel-default" style="background-color:#eee">
@@ -21,7 +29,83 @@
             <center><b>Tabel Gagal Diupdate!</b></center>z
             </div>";
            }
+
+           function cekStatus($conn, $id_kamar, $tgl_checkin, $tgl_checkout)
+          {
+            $query_cekStatus = "select count(*) as count
+                                from transaksi_sewakamar tr,
+                                  ( select *
+                                  from menyewa
+                                  where id_kamar='$id_kamar') m
+                                where m.id_transaksi = tr.id_transaksi
+                                and (
+                                  (
+                                    tgl_checkin <= to_date('$tgl_checkin','dd-mm-yyyy')
+                                    and
+                                    tgl_checkout > to_date('$tgl_checkin','dd-mm-yyyy')
+                                  )
+                                  or
+                                  (
+                                    tgl_checkin < to_date('$tgl_checkout', 'dd-mm-yyyy')
+                                    and
+                                    tgl_checkout >= to_date('$tgl_checkout', 'dd-mm-yyyy')
+                                  )
+                                  or
+                                  (
+                                    tgl_checkin >= to_date('$tgl_checkin', 'dd-mm-yyyy')
+                                    and
+                                    tgl_checkout <= to_date('$tgl_checkout', 'dd-mm-yyyy')
+                                  )
+                                )";
+            $query_cekStatus_parse = oci_parse($conn, $query_cekStatus);
+            oci_execute($query_cekStatus_parse);
+            $row = oci_fetch_array($query_cekStatus_parse);
+            if ($row['COUNT'] != '0')
+              return 1; // sudah dipesan
+            else
+              return 0; // kosong
+
+          }
+          if (isset($_POST['date-check-in']) and isset($_POST['date-check-out']))
+          {
+            $_SESSION['date-check-in'] = $_POST['date-check-in'];
+            $_SESSION['date-check-out'] = $_POST['date-check-out'];
+            $tgl_checkin = $_SESSION['date-check-in'];
+            $tgl_checkout = $_SESSION['date-check-out'];
+            $_SESSION['flag-pick-date'] = true;
+          }
         ?>
+
+        <?php
+        if (!isset($_SESSION['flag-pick-date']))
+        {?>
+
+           <!-- Input tanggal check-in check-out -->
+          <form class="form-horizontal" role="form" action="<?php $_PHP_SELF ?>" method="POST">
+          <div class="form-group">
+            <label class="control-label col-sm-2" for="date-check-in"> Check in : </label>
+            <div class="col-sm-10">
+              <input type="text" class="span2 form-control" value id="date-check-in" name="date-check-in" data-date-format="dd-mm-yyyy" required>
+            </div>
+          </div>
+          <div class="form-group">
+            <label class="control-label col-sm-2" for="date-check-out"> Check out : </label>
+            <div class="col-sm-10">
+              <input type="text" class="span2 form-control" value id="date-check-out" name="date-check-out" data-date-format="dd-mm-yyyy" required>
+            </div>
+          </div>
+          <div class="form-group">
+            <div class="col-sm-offset-2 col-sm-10">
+              <button type="submit" class="btn btn-default">Submit</button>
+            </div>
+          </div>
+        </form>
+        <!-- Input tanggal check-in check-out -->
+        <?php
+        }
+        else
+        {
+          ?>
           <table class="table table-hover table-bordered table-striped">
             <thead>
               <tr>
@@ -54,11 +138,11 @@
                 <td>
                   <div>
                     <form method="POST" action="wisma-pilih.php">
-                      <?php
-                      if (strcmp($row['STATUS_KAMAR'],"KOSONG")==1)
-                        $tombol= "btn btn-primary disabled";
+                     <?php
+                      if (cekStatus($conn, $row['ID_KAMAR'], $tgl_checkin, $tgl_checkout) ==  1)
+                        $tombol = "btn btn-primary disabled";
                       else
-                        $tombol= "btn btn-primary";
+                        $tombol = "btn btn-primary";
                       echo '
                       <center>
                         <button type="submit" class="'.$tombol.'" name="id_kamar" value="'.$row['ID_KAMAR'].'">Pilih</button>
@@ -101,6 +185,10 @@
             <a href="#">  <button class="btn">KEMBALI</button>
           </a> </div>
 
+        <?php
+          unset($_SESSION['flag-pick-date']);
+        }
+        ?>
           <div>
            <?php
               include "coba.php";
@@ -129,5 +217,40 @@
 </div>
 
 
+<!-- datepicker -->
+<script type="text/javascript">
+var nowTemp = new Date();
+var now = new Date(nowTemp.getFullYear(), nowTemp.getMonth(), nowTemp.getDate(), 0, 0, 0, 0);
+
+var checkin = $('#date-check-in').datepicker({
+  onRender: function(date) {
+    return date.valueOf() < now.valueOf() ? 'disabled' : '';
+  }
+}).on('changeDate', function(ev) {
+  if (ev.date.valueOf() > checkout.date.valueOf()) {
+    var newDate = new Date(ev.date)
+    newDate.setDate(newDate.getDate() + 1);
+    checkout.setValue(newDate);
+  }
+  checkin.hide();
+  $('#date-check-out')[0].focus();
+}).data('datepicker');
+var checkout = $('#date-check-out').datepicker({
+  onRender: function(date) {
+    return date.valueOf() <= checkin.date.valueOf() ? 'disabled' : '';
+  }
+}).on('changeDate', function(ev) {
+  checkout.hide();
+}).data('datepicker');
+</script>
+<!-- datepicker -->
+
+<style type="text/css">
+.form-horizontal {
+  max-width: 700px;
+  padding: 15px;
+  margin: 0 auto;
+}
+</style>
 
 <?php include("template/footer.php");?>
